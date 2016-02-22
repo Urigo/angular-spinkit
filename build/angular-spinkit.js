71d6197e681f86970ff4917ab593159090cd47c3
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-02-22 15:26:32 +0200

    chore(release): Release 0.3.4 to Npm and Bower

--- build/angular-spinkit.js
+++ build/angular-spinkit.js
@@ -154,183 +154,116 @@ angular.module('angular-spinkit').run(['$templateCache', function($templateCache
   'use strict';
 
   $templateCache.put('src/templates/chasingDotsSpinner.html',
-    "<div class=\"chasing-dots-spinner\">\r" +
-    "\n" +
-    "  <div class=\"dot1\"></div>\r" +
-    "\n" +
-    "  <div class=\"dot2\"></div>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "<div class=\"chasing-dots-spinner\">\n" +
+    "  <div class=\"dot1\"></div>\n" +
+    "  <div class=\"dot2\"></div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('src/templates/circleSpinner.html',
-    "<div class=\"spinning-dots-spinner\">\r" +
-    "\n" +
-    "  <div class=\"spinner-container container1\">\r" +
-    "\n" +
-    "    <div class=\"circle1\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle2\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle3\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle4\"></div>\r" +
-    "\n" +
-    "  </div>\r" +
-    "\n" +
-    "  <div class=\"spinner-container container2\">\r" +
-    "\n" +
-    "    <div class=\"circle1\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle2\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle3\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle4\"></div>\r" +
-    "\n" +
-    "  </div>\r" +
-    "\n" +
-    "  <div class=\"spinner-container container3\">\r" +
-    "\n" +
-    "    <div class=\"circle1\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle2\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle3\"></div>\r" +
-    "\n" +
-    "    <div class=\"circle4\"></div>\r" +
-    "\n" +
-    "  </div>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "<div class=\"spinning-dots-spinner\">\n" +
+    "  <div class=\"spinner-container container1\">\n" +
+    "    <div class=\"circle1\"></div>\n" +
+    "    <div class=\"circle2\"></div>\n" +
+    "    <div class=\"circle3\"></div>\n" +
+    "    <div class=\"circle4\"></div>\n" +
+    "  </div>\n" +
+    "  <div class=\"spinner-container container2\">\n" +
+    "    <div class=\"circle1\"></div>\n" +
+    "    <div class=\"circle2\"></div>\n" +
+    "    <div class=\"circle3\"></div>\n" +
+    "    <div class=\"circle4\"></div>\n" +
+    "  </div>\n" +
+    "  <div class=\"spinner-container container3\">\n" +
+    "    <div class=\"circle1\"></div>\n" +
+    "    <div class=\"circle2\"></div>\n" +
+    "    <div class=\"circle3\"></div>\n" +
+    "    <div class=\"circle4\"></div>\n" +
+    "  </div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('src/templates/cubeGridSpinner.html',
-    "<div class=\"cube-grid-spinner\">\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
-    "  <div class=\"cube\"></div>\r" +
-    "\n" +
+    "<div class=\"cube-grid-spinner\">\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
+    "  <div class=\"cube\"></div>\n" +
     "</div>"
   );
 
 
   $templateCache.put('src/templates/doubleBounceSpinner.html',
-    "<div class=\"double-bounce-spinner\">\r" +
-    "\n" +
-    "  <div class=\"double-bounce1\"></div>\r" +
-    "\n" +
-    "  <div class=\"double-bounce2\"></div>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "<div class=\"double-bounce-spinner\">\n" +
+    "  <div class=\"double-bounce1\"></div>\n" +
+    "  <div class=\"double-bounce2\"></div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('src/templates/fadingCircleSpinner.html',
-    "<div class=\"fading-circle-spinner\">\r" +
-    "\n" +
-    "  <div class=\"fading-circle1 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle2 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle3 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle4 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle5 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle6 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle7 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle8 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle9 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle10 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle11 fading-circle\"></div>\r" +
-    "\n" +
-    "  <div class=\"fading-circle12 fading-circle\"></div>\r" +
-    "\n" +
+    "<div class=\"fading-circle-spinner\">\n" +
+    "  <div class=\"fading-circle1 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle2 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle3 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle4 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle5 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle6 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle7 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle8 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle9 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle10 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle11 fading-circle\"></div>\n" +
+    "  <div class=\"fading-circle12 fading-circle\"></div>\n" +
     "</div>"
   );
 
 
   $templateCache.put('src/templates/pulseSpinner.html',
-    "<div class=\"pulse-spinner\"></div>\r" +
-    "\n"
+    "<div class=\"pulse-spinner\"></div>\n"
   );
 
 
   $templateCache.put('src/templates/rotatingPlaneSpinner.html',
-    "<div class=\"three-dots-row-spinner\"></div>\r" +
-    "\n"
+    "<div class=\"three-dots-row-spinner\"></div>\n"
   );
 
 
   $templateCache.put('src/templates/threeBounceSpinner.html',
-    "<div class=\"three-bounce-spinner\">\r" +
-    "\n" +
-    "  <div class=\"bounce1\"></div>\r" +
-    "\n" +
-    "  <div class=\"bounce2\"></div>\r" +
-    "\n" +
-    "  <div class=\"bounce3\"></div>\r" +
-    "\n" +
+    "<div class=\"three-bounce-spinner\">\n" +
+    "  <div class=\"bounce1\"></div>\n" +
+    "  <div class=\"bounce2\"></div>\n" +
+    "  <div class=\"bounce3\"></div>\n" +
     "</div>"
   );
 
 
   $templateCache.put('src/templates/wanderingCubesSpinner.html',
-    "<div class=\"wandering-cubes-spinner\"></div>\r" +
-    "\n"
+    "<div class=\"wandering-cubes-spinner\"></div>\n"
   );
 
 
   $templateCache.put('src/templates/waveSpinner.html',
-    "<div class=\"wave-spinner\">\r" +
-    "\n" +
-    "  <div class=\"rect1\"></div>\r" +
-    "\n" +
-    "  <div class=\"rect2\"></div>\r" +
-    "\n" +
-    "  <div class=\"rect3\"></div>\r" +
-    "\n" +
-    "  <div class=\"rect4\"></div>\r" +
-    "\n" +
-    "  <div class=\"rect5\"></div>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "<div class=\"wave-spinner\">\n" +
+    "  <div class=\"rect1\"></div>\n" +
+    "  <div class=\"rect2\"></div>\n" +
+    "  <div class=\"rect3\"></div>\n" +
+    "  <div class=\"rect4\"></div>\n" +
+    "  <div class=\"rect5\"></div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('src/templates/wordPressSpinner.html',
-    "<div class=\"word-press-spinner\">\r" +
-    "\n" +
-    "  <span class=\"inner-circle\"></span>\r" +
-    "\n" +
+    "<div class=\"word-press-spinner\">\n" +
+    "  <span class=\"inner-circle\"></span>\n" +
     "</div>"
   );
 
